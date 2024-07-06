d3fdb9c121f9734949df204c419595272fdadc9a
yadoollahi <$m.r.yadoollahi@gmail.com>
2024-07-06 10:03:05 +0330

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,59 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+
+import { HtmlProps } from "next/dist/shared/lib/html-context.shared-runtime";
 
 export default function Home() {
+
+
+  const Btn = (props: any) => {
+    return <button style={{ height: 60, width: 60 }} onClick={() => {
+      
+      let op = props.txt;
+      let el = (document.getElementById("txt") as HTMLInputElement);
+
+      if (op == "clear") {
+        el.value = "";
+        return;
+      }
+      if (op == "=") {
+        el.value = eval(el.value)
+      }
+      else {
+        el.value = el.value + op;
+      }
+
+    }} > {props.txt}</button>
+  }
+
+
+
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div>
+      <input id="txt" style={{ height: 40, width: 240 }}></input>
+      <br />
+      <Btn txt="-" />
+      <Btn txt="1" />
+      <Btn txt="2" />
+      <Btn txt="3" />
+      <br />
+      <Btn txt="+" />
+      <Btn txt="4" />
+      <Btn txt="5" />
+      <Btn txt="6" />
+      <br />
+      <Btn txt="0" />
+      <Btn txt="7" />
+      <Btn txt="8" />
+      <Btn txt="9" />
+      <br />
+      <Btn txt="="/>
+      <Btn txt="clear"/>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+      <br />
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
